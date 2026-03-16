3d559f95c9c52afa10acbfa9bdd1581e2362f044
lucy-413 <theezik125@gmail.com>
2026-03-16 14:26:02 +0100

    Rainbow logo animation

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 
+
+// Kolorowe Logo
+const middleParts = document.querySelectorAll('.ascii-middle');
+
+middleParts.forEach(span => {
+
+  span.addEventListener('mouseenter', () => {
+    middleParts.forEach(s => s.classList.add('is-hovered'));
+  });
+
+
+  span.addEventListener('mouseleave', () => {
+    middleParts.forEach(s => s.classList.remove('is-hovered'));
+  });
+});
+
 function loadHTML(id, filename) {
   fetch(filename)
     .then(response => {
@@ -8,6 +24,8 @@ function loadHTML(id, filename) {
       document.getElementById(id).innerHTML = text;
     })
 }
+
+// Ładowanie rzeczy
 
 document.addEventListener("DOMContentLoaded", function() {
   loadHTML("nav-placeholder", "nav.html");
